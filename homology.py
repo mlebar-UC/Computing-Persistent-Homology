7dfa6886fd3dba98424130356a0c47abab3517de
mlebar-UC <mlebar@uchicago.edu>
2025-12-08 13:23:38 -0600

    Got code workinggit add .git add .

---
+++ homology.py
@@ -0,0 +1,165 @@
+import numpy as np
+import gudhi as gd
+import matplotlib.pyplot as plt
+import math
+from matplotlib.patches import Patch
+
+
+def create_persistence_complex(dataset, complex_type="alpha", max_dim=3):
+    """
+    Given a dataset, constructs a persistence complex out of that dataset
+    """
+    if complex_type == "alpha":
+        complex = gd.AlphaComplex(points=dataset)
+    elif complex_type == "rips":
+        complex = gd.RipsComplex(points=dataset)
+    else:
+        raise TypeError("Complex type must be 'rips' or 'alpha'")
+
+    pc = []
+    sd = {}
+
+    stree = complex.create_simplex_tree()
+    stree.prune_above_dimension(max_dim)
+
+    for i, (vertices, squared_circumradii) in enumerate(stree.get_filtration()):
+        deg = np.sqrt(squared_circumradii)
+        s = Simplex(vertices, deg)
+        pc.append(s)
+        sd[tuple(vertices)] = [False, i]
+
+    return pc, sd, stree
+
+
+class Simplex:
+    def __init__(self, vertices, degree):
+        self.vertices = vertices
+        self.dim = len(vertices) - 1
+        self.deg = degree
+
+    def __str__(self):
+        return f"Vertices: {self.vertices}, degree: {self.deg} at index {self.idx}"
+
+    def compute_reduced_boundary_chain(self, simplex_dictionary):
+        boundary_chain = set()
+
+        if self.dim == 0:
+            return boundary_chain
+
+        for i, _ in enumerate(self.vertices):
+            face = tuple(self.vertices[:i] + self.vertices[i + 1 :])
+            # only add marked faces
+            if simplex_dictionary[face][0]:
+                boundary_chain.add(face)
+
+        return boundary_chain
+
+    def mark(self, simplex_dictionary):
+        simplex_dictionary[tuple(self.vertices)][0] = True
+
+    def check_if_marked(self, simplex_dictionary):
+        return simplex_dictionary[tuple(self.vertices)][0]
+
+
+def max_index(boundary_chain, simplex_dictionary):
+    max_idx = -1
+    for face in boundary_chain:
+        idx = simplex_dictionary[face][1]
+        if idx > max_idx:
+            max_idx = idx
+    return max_idx
+
+
+def remove_pivot_rows(simplex, simplex_dictionary, T):
+    d = simplex.compute_reduced_boundary_chain(simplex_dictionary)
+
+    while len(d) > 0:
+        i = max_index(d, simplex_dictionary)
+        if T[i] is None:
+            break
+        d = d ^ T[i]
+
+    return d
+
+
+def compute_intervals(
+    persistence_complex, simplex_dictionary, max_dim=3, remove_small_intervals=True
+):
+    """
+    Computes persistence homology intervals given a persistence complex, assumed to be in filtration order. We also assume the maximum dimension of the persistence complex is one more than the max dimension of the algorithm.
+    """
+
+    # initialize data structures
+    interval_sets = [[] for _ in range(max_dim)]
+    T = [None for _ in range(len(persistence_complex))]
+
+    # the simplex is in filtration order, which is important for the use of max_index
+    for j, simplex_j in enumerate(persistence_complex):
+        d = remove_pivot_rows(simplex_j, simplex_dictionary, T)
+        if len(d) == 0:
+            # this means that the k-simplex did not "kill off" a k-1 simplex, so we mark it as a creator of a k-cycle
+            simplex_j.mark(simplex_dictionary)
+        else:
+            # otherwise we want to record that it killed a cycle
+            i = max_index(d, simplex_dictionary)
+            simplex_i = persistence_complex[i]
+            k = simplex_i.dim
+            T[i] = d
+            if simplex_j.deg > simplex_i.deg or not remove_small_intervals:
+                interval_sets[k].append((simplex_i.deg, simplex_j.deg))
+
+    for j, simplex in enumerate(persistence_complex):
+        if simplex.check_if_marked(simplex_dictionary) and T[j] is None:
+            # this means the simplex was marked as creating a cycle, but never marked as having that cycle killed. So we add an interval to infinity here
+            k = simplex.dim
+            interval_sets[k].append((simplex.deg, float("inf")))
+
+    return interval_sets
+
+
+def display_intervals(persistence_interval_set, max_intervals=20, min_distance=0.01):
+    """
+    Creates barcode display for given persistence homology intervals for a single dimension
+    """
+    dims = len(persistence_interval_set)
+
+    # associate intervals with their dimensions and length
+    interval_lengths = {}
+    interval_dims = {}
+    intervals = []
+    end = 0
+    for i, interval_set in enumerate(persistence_interval_set):
+        for a, b in interval_set:
+            if b > end and not math.isinf(b):
+                end = b
+            if b - a > min_distance:
+                interval_lengths[(a, b)] = b - a
+                interval_dims[(a, b)] = i
+                intervals.append((a, b))
+
+    intervals.sort(key=lambda interval: interval_lengths[interval], reverse=True)
+    intervals = intervals[:max_intervals]
+
+    num_intervals = len(intervals)
+
+    fig, ax = plt.subplots(figsize=(6, num_intervals * 0.2))
+
+    color_map = {0: "red", 1: "blue", 2: "green"}
+    legend_elements = [
+        Patch(facecolor="red", edgecolor="red", label="Dimension 0"),
+        Patch(facecolor="blue", edgecolor="blue", label="Dimension 1"),
+        Patch(facecolor="green", edgecolor="green", label="Dimension 2"),
+    ]
+
+    ax.barh(
+        np.arange(num_intervals),
+        [b - a if b < end else end - a for (a, b) in intervals],
+        left=[a for (a, _) in intervals],
+        height=0.7,
+        color=[color_map[interval_dims[(a, b)]] for (a, b) in intervals],
+    )
+    ax.legend(handles=legend_elements)
+
+    ax.get_yaxis().set_visible(False)
+
+    plt.show()
--- test_shapes.py
+++ test_shapes.py
@@ -5,11 +5,15 @@ import numpy as np
 from datasets import load_dataset
 
 
-def plot_3d_shape(X, Y, Z, cmap="viridis"):
+def plot_3d_points(X, Y, Z, cmap="viridis"):
     fig = plt.figure(figsize=(5, 4))
     ax = plt.axes(projection="3d")
     ax.scatter(X, Y, Z, cmap=cmap)
     plt.show()
+
+
+def plot_3d_shape(shape):
+    plot_3d_points(shape[:, 0], shape[:, 1], shape[:, 2])
 
 
 def plot_surface(X, Y, Z, surface, cmap="viridis"):
@@ -31,3 +35,26 @@ def get_genus_g_surface(g):
 
     surface = trimesh.load(path)
     return surface, surface.sample(1000)
+
+
+def sample_points_from_sphere(center, r=1, num_points=1000):
+    """'
+    Approach taken from https://stackoverflow.com/questions/33976911/generate-a-random-sample-of-points-distributed-on-the-surface-of-a-unit-sphere
+    Very simple and clever idea, to get points on a sphere, generate vectors randomly and then normalize them.
+    """
+    vectors = np.random.randn(num_points, 3)
+    vectors = r * vectors / np.linalg.norm(vectors, axis=1, keepdims=True)
+    vectors += center
+
+    return vectors
+
+
+def get_n_spheres(n):
+    # generate n points at least distance 2 from each other
+    # sample from spheres
+    points = sample_points_from_sphere([0, 0, 0])
+    for i in range(1, n):
+        points = np.concatenate(
+            (points, sample_points_from_sphere([2 * i, 0, 0])), axis=0
+        )
+    return points
