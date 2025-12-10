de4f835f6e20facc43d679bf1694d89d3ef06004
mlebar-UC <mlebar@uchicago.edu>
2025-12-09 21:51:20 -0600

    Complete project

---
+++ README.md
@@ -0,0 +1,9 @@
+Contains all files for my final project for MPCS 53112, Advanced Data Analytics with Amitabh Chaudhary. 
+
+Code is an implementation of an algorithm for persitence homology, plus material for testing. "homology.py" contains the algorithm itself, along with helper methods as well as post and pre-processing functions. "test_shape.py" contains code to produce shapes with which to test the algorithm. "test.ipynb" contains a few tests of the code with brief commentary. 
+
+Presentations includes slides for two in-class presentations for this project.
+
+This repo also contains my final report for the project, which includes detailed explanation of the code and necessary theoretical exposition.
+
+Enjoy.
--- code/test_shapes.py
+++ code/test_shapes.py
@@ -2,28 +2,39 @@ import matplotlib.pyplot as plt
 from huggingface_hub import hf_hub_download, list_repo_files
 import trimesh
 import numpy as np
-from datasets import load_dataset
 
 
 def plot_3d_points(X, Y, Z, cmap="viridis"):
-    fig = plt.figure(figsize=(5, 4))
+    """
+    Plots a point cloud in 3d space.
+    """
+    _ = plt.figure(figsize=(5, 4))
     ax = plt.axes(projection="3d")
     ax.scatter(X, Y, Z, cmap=cmap)
     plt.show()
 
 
 def plot_3d_shape(shape):
+    """
+    Unpacks points in from a shape to call plot_3d_points.
+    """
     plot_3d_points(shape[:, 0], shape[:, 1], shape[:, 2])
 
 
 def plot_surface(X, Y, Z, surface, cmap="viridis"):
-    fig = plt.figure(figsize=(5, 4))
+    """
+    Plots a 3d trimesh surface. Assumes the surface is from the trimesh library
+    """
+    _ = plt.figure(figsize=(5, 4))
     ax = plt.axes(projection="3d")
     ax.plot_trisurf(X, Y, Z, triangles=surface.faces, cmap=cmap)
     plt.show()
 
 
 def get_genus_g_surface(g):
+    """
+    Produces a surface of genus g for testing. Returns a surface that can be plotted (this was mainly because it's fun to look at these surfaces with fancy cmaps than anything really practical) and a set of points sampled from that surface.
+    """
     file_names = list_repo_files("appliedgeometry/EuLearn", repo_type="dataset")
     path = None
 
@@ -38,7 +49,7 @@ def get_genus_g_surface(g):
 
 
 def sample_points_from_sphere(center, r=1, num_points=1000):
-    """'
+    """
     Approach taken from https://stackoverflow.com/questions/33976911/generate-a-random-sample-of-points-distributed-on-the-surface-of-a-unit-sphere
     Very simple and clever idea, to get points on a sphere, generate vectors randomly and then normalize them.
     """
@@ -50,6 +61,9 @@ def sample_points_from_sphere(center, r=1, num_points=1000):
 
 
 def get_n_spheres(n):
+    """
+    Returns a point cloud samples from n-spheres, any pair of which intersect at at most point.
+    """
     # generate n points at least distance 2 from each other
     # sample from spheres
     points = sample_points_from_sphere([0, 0, 0])
